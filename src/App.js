0a6be1dab4a648b18b429d7ce589ee26818bff16
David Athobe <dathobek@gmail.com>
2020-10-27 15:20:29 +0100

    page signout completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import './App.css';
 import Navigation from './components/navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+
+const particleOptions = {
+  particles: {
+    numbers: {
+      value: 30,
+      density: {
+        enable:true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particle'
+        params ={particleOptions}
+      />
        <Navigation />
-       {/* <Logo />
+       <Logo />
+       <Rank/>
        <ImageLinkForm />
-       <FaceRecognition /> */}
+       {/* <FaceRecognition /> */}
     </div>
   );
 }
