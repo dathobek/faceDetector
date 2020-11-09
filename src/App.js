5e4ddbfd09d1de5bdc89dda41db9c8ccb11137af
David Athobe <dathobek@gmail.com>
2020-11-09 13:11:52 +0100

    forms

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Navigation from './components/navigation/Navigation';
+import SignIn from './components/signin/signin';
+import Register from './components/Register/Register';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/faceRecog/FaceRecognition ';
@@ -30,29 +32,48 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
+      route: 'signin',
     }
+  }
+
+  calculateFaceLocation = (data) =>{
+    const clarifiaFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifiaFace.left_col * width,
+      topRow: clarifiaFace.top_Row * height,
+      rightCol: width - (clarifiaFace.right_col * width),
+      bottomRow: height - (clarifiaFace.button_row * height)
+    }
+  }
+
+  displayFaceBox = (box) =>{
+    console.log(box);
+    this.setState({box:box});
   }
 
   onInputChange = (event) =>{
     this.setState({input: event.target.value})
   }
 
+  //this button uses clarifia face detector Api
   onButtonChange = () =>{
     this.setState({imageUrl: this.state.input})
     app.models
     .predict(
-      Clarifai.FACE_DETECT_MODEL,
+      'c0c0ac362b03416da06ab3fa36fb58e3',
       this.state.input)
-    .then(
-      function(response){
-        console.log(response);
-      },
-      function(err){
-        //there was an error
-      }
-    );
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err))
+     
+  }
 
+  onRouteChange =(route)=>{
+    this.setState({route: route});
   }
 
   render(){
@@ -61,11 +82,19 @@ class App extends Component {
         <Particles className='particle'
           params ={particleOptions}
         />
-         <Navigation />
-         <Logo />
-         <Rank/>
-         <ImageLinkForm onInputChange={this.onInputChange} onButtonChange={this.onButtonChange}/>
-         <FaceRecognition imageUrl={this.state.imageUrl}/>
+          <Navigation onRouteChange={this.onRouteChange}/>
+         { this.state.route === 'home' 
+          ? <div>
+          <Logo />
+          <Rank/>
+          <ImageLinkForm onInputChange={this.onInputChange} onButtonChange={this.onButtonChange}/>
+          <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+         </div>
+         : (  this.state.route === 'signin'
+             ? <SignIn onRouteChange={this.onRouteChange}/> 
+             : <Register onRouteChange={this.onRouteChange}/>
+           )   
+        }
       </div>
     );
   }
